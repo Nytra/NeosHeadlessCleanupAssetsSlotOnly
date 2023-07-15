22544d6f7bd9003a37df3a9a82b90dcb932c5d94
Sam Scott <samueltscott@gmail.com>
2023-07-15 04:09:57 +0100

    Made it loop until finished

--- HeadlessCustomAssetCleanup/HeadlessCustomAssetCleanup.cs
+++ HeadlessCustomAssetCleanup/HeadlessCustomAssetCleanup.cs
@@ -7,68 +7,89 @@ using System.Reflection;
 
 namespace HeadlessCustomAssetCleanup
 {
-    public class HeadlessCustomAssetCleanup : NeosMod
-    {
-        public override string Name => "HeadlessCustomAssetCleanup";
-        public override string Author => "Nytra";
-        public override string Version => "1.0.0";
-        public override string Link => "https://github.com/Nytra/NeosHeadlessCustomAssetCleanup";
+	public class HeadlessCustomAssetCleanup : NeosMod
+	{
+		public override string Name => "HeadlessCustomAssetCleanup";
+		public override string Author => "Nytra";
+		public override string Version => "1.1.0";
+		public override string Link => "https://github.com/Nytra/NeosHeadlessCustomAssetCleanup";
 
-        private static FieldInfo lastAutoSaveField = AccessTools.Field(typeof(WorldConfiguration), "_lastAutoSave");
-        private static FieldInfo lastAssetCleanupField = AccessTools.Field(typeof(WorldConfiguration), "_lastAssetCleanup");
+		private static FieldInfo lastAutoSaveField = AccessTools.Field(typeof(WorldConfiguration), "_lastAutoSave");
+		private static FieldInfo lastAssetCleanupField = AccessTools.Field(typeof(WorldConfiguration), "_lastAssetCleanup");
 
-        public override void OnEngineInit()
-        {
-            Harmony harmony = new Harmony("owo.Nytra.HeadlessCustomAssetCleanup");
-            Type neosHeadless = AccessTools.TypeByName("NeosHeadless.Program");
-            if (neosHeadless != null)
-            {
-                harmony.PatchAll();
-            }
-        }
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony("owo.Nytra.HeadlessCustomAssetCleanup");
+			Type neosHeadless = AccessTools.TypeByName("NeosHeadless.Program");
+			if (neosHeadless != null)
+			{
+				harmony.PatchAll();
+			}
+		}
 
-        // A transpiler would be better here
-        // ... but I'm lazy
+		private static void CustomCleanupAssets(World w)
+		{
+			int count = 1;
+			int iterations = 0;
+			//Debug("Entering loop!");
+			while (count > 0 && iterations < 25)
+			{
+				//Debug("Loop iteration started!");
+				List<IAssetProvider> componentsInChildren = w.AssetsSlot.GetComponentsInChildren((IAssetProvider p) => p.AssetReferenceCount == 0);
+				count = componentsInChildren.Count;
 
-        [HarmonyPatch(typeof(WorldConfiguration), "InternalRunUpdate")]
-        class HeadlessCustomAssetCleanupPatch
-        {
-            public static bool Prefix(WorldConfiguration __instance)
-            {
-                if (!__instance.World.IsAuthority)
-                {
-                    return false;
-                }
-                if (__instance.AutoSaveEnabled.Value && (DateTime.UtcNow - (DateTime)lastAutoSaveField.GetValue(__instance)).TotalMinutes >= (double)__instance.AutoSaveInterval.Value)
-                {
-                    lastAutoSaveField.SetValue(__instance, DateTime.UtcNow);
-                    Userspace.SaveWorldAuto(__instance.World, SaveType.Overwrite, exitOnSave: false);
-                }
-                if (__instance.AutoCleanupEnabled.Value && (DateTime.UtcNow - (DateTime)lastAssetCleanupField.GetValue(__instance)).TotalSeconds >= (double)__instance.AutoCleanupInterval.Value)
-                {
-                    Debug("Running custom asset cleanup!");
+				componentsInChildren.ForEach(delegate (IAssetProvider p)
+				{
+					p.Destroy();
+				});
 
-                    lastAssetCleanupField.SetValue(__instance, DateTime.UtcNow);
+				count += WorldOptimizer.CleanupEmptySlots(w.AssetsSlot);
+				iterations++;
+			}
+			//Debug("Finished loop!");
+		}
 
-                    List<IAssetProvider> componentsInChildren = __instance.World.AssetsSlot.GetComponentsInChildren((IAssetProvider p) => p.AssetReferenceCount == 0);
-                    componentsInChildren.ForEach(delegate (IAssetProvider p)
-                    {
-                        p.Destroy();
-                    });
+		// A transpiler would be better here
+		// ... but I'm lazy
 
-                    WorldOptimizer.CleanupEmptySlots(__instance.World.AssetsSlot);
+		[HarmonyPatch(typeof(WorldConfiguration), "InternalRunUpdate")]
+		class HeadlessCustomAssetCleanupPatch
+		{
+			public static bool Prefix(WorldConfiguration __instance)
+			{
+				if (!__instance.World.IsAuthority)
+				{
+					return false;
+				}
+				if (__instance.AutoSaveEnabled.Value && (DateTime.UtcNow - (DateTime)lastAutoSaveField.GetValue(__instance)).TotalMinutes >= (double)__instance.AutoSaveInterval.Value)
+				{
+					lastAutoSaveField.SetValue(__instance, DateTime.UtcNow);
+					Userspace.SaveWorldAuto(__instance.World, SaveType.Overwrite, exitOnSave: false);
+				}
+				if (__instance.AutoCleanupEnabled.Value && (DateTime.UtcNow - (DateTime)lastAssetCleanupField.GetValue(__instance)).TotalSeconds >= (double)__instance.AutoCleanupInterval.Value)
+				{
+					lastAssetCleanupField.SetValue(__instance, DateTime.UtcNow);
 
-                    // ORIGINAL CODE BELOW
+					Debug("Running custom asset cleanup!");
 
-                    //if (__instance.InputInterface.HeadDevice == HeadOutputDevice.Headless)
-                    //{
-                        //MaterialOptimizer.DeduplicateMaterials(__instance.World);
-                        //WorldOptimizer.DeduplicateStaticProviders(__instance.World);
-                    //}
-                    //WorldOptimizer.CleanupAssets(__instance.World, ignoreNonpersistentUsers: false, WorldOptimizer.CleanupMode.Destroy);
-                }
-                return false;
-            }
-        }
-    }
+					CustomCleanupAssets(__instance.World);
+
+					__instance.World.RunInUpdates(7, delegate { CustomCleanupAssets(__instance.World); });
+
+					//Debug("Finished custom asset cleanup!");
+
+					// ORIGINAL CODE BELOW
+
+					//_lastAssetCleanup = DateTime.UtcNow;
+					//if (base.InputInterface.HeadDevice == HeadOutputDevice.Headless)
+					//{
+					//    MaterialOptimizer.DeduplicateMaterials(base.World);
+					//    WorldOptimizer.DeduplicateStaticProviders(base.World);
+					//}
+					//WorldOptimizer.CleanupAssets(base.World, ignoreNonpersistentUsers: false, WorldOptimizer.CleanupMode.Destroy);
+				}
+				return false;
+			}
+		}
+	}
 }
